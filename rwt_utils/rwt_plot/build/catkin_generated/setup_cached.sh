1466d80323afda868899d67cea817f1386407a16
Ryohei Ueda <garaemon@ubuntu.(none)>
2013-10-13 10:50:26 -0700

    adding wrapper package for roslibjs, ros2djs and ros3djs

---
+++ rwt_utils/rwt_plot/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+export CATKIN_TEST_RESULTS_DIR="/home/garaemon/ros/groovy/garaemon-ros-pkg/rwt_utils/rwt_plot/build/test_results"
+export ROS_TEST_RESULTS_DIR="/home/garaemon/ros/groovy/garaemon-ros-pkg/rwt_utils/rwt_plot/build/test_results"
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/garaemon/ros/groovy/garaemon-ros-pkg/rwt_utils/rwt_plot/build/devel:$CMAKE_PREFIX_PATH"
+export CPATH="/home/garaemon/ros/groovy/garaemon-ros-pkg/rwt_utils/rwt_plot/build/devel/include:$CPATH"
+export LD_LIBRARY_PATH="/home/garaemon/ros/groovy/garaemon-ros-pkg/rwt_utils/rwt_plot/build/devel/lib:$LD_LIBRARY_PATH"
+export PATH="/home/garaemon/ros/groovy/garaemon-ros-pkg/rwt_utils/rwt_plot/build/devel/bin:/opt/ros/groovy/bin:/home/garaemon/gprog/elastic-mapreduce-ruby:/usr/local/mahout/bin:/usr/local/bin:/Library/Haskell/bin:/usr/local/heroku/bin:/opt/android-sdk/tools:/opt/android-sdk/tools:/home/garaemon/gprog/elastic-mapreduce-ruby:/usr/local/mahout/bin:/usr/local/bin:/Library/Haskell/bin:/usr/local/heroku/bin:/opt/android-sdk/tools:/opt/android-sdk/tools:/home/garaemon/gprog/elastic-mapreduce-ruby:/usr/local/mahout/bin:/usr/local/bin:/Library/Haskell/bin:/usr/local/heroku/bin:/home/garaemon/.nvm/v0.10.3/bin:/opt/android-sdk/tools:/opt/android-sdk/tools:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/home/garaemon/gprog/scan-scripts/bin:/home/garaemon/gprog/platex-create-pkg:/home/garaemon/gprog/emacs-settings:/home/garaemon/gprog/git-your-emacs:/home/garaemon/gprog/heroku-nodekit:/usr/local/Cellar/pypy/1.9/bin/:/home/garaemon/gprog/minify-make:/Applications/SenchaSDKTools-2.0.0-beta3:/home/garaemon/gprog/jq:/usr/local/share/python:/home/garaemon/gprog/snapshotter/bin:/home/garaemon/gprog/s3sync/:/home/garaemon/gprog/scan-scripts/bin:/home/garaemon/gprog/platex-create-pkg:/home/garaemon/gprog/emacs-settings:/home/garaemon/gprog/git-your-emacs:/home/garaemon/gprog/heroku-nodekit:/usr/local/Cellar/pypy/1.9/bin/:/home/garaemon/gprog/minify-make:/Applications/SenchaSDKTools-2.0.0-beta3:/home/garaemon/gprog/jq:/usr/local/share/python:/home/garaemon/gprog/snapshotter/bin:/home/garaemon/gprog/s3sync/:/home/garaemon/gprog/scan-scripts/bin:/home/garaemon/gprog/platex-create-pkg:/home/garaemon/gprog/emacs-settings:/home/garaemon/gprog/git-your-emacs:/home/garaemon/gprog/heroku-nodekit:/usr/local/Cellar/pypy/1.9/bin/:/home/garaemon/gprog/minify-make:/Applications/SenchaSDKTools-2.0.0-beta3:/home/garaemon/gprog/jq:/usr/local/share/python:/home/garaemon/gprog/snapshotter/bin:/home/garaemon/gprog/s3sync/"
+export PKG_CONFIG_PATH="/home/garaemon/ros/groovy/garaemon-ros-pkg/rwt_utils/rwt_plot/build/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
+export PYTHONPATH="/home/garaemon/ros/groovy/garaemon-ros-pkg/rwt_utils/rwt_plot/build/devel/lib/python2.7/dist-packages:/opt/ros/groovy/lib/python2.7/dist-packages:/usr/local/lib/python2.7/site-packages:/usr/local/lib/python2.7/site-packages:/usr/local/lib/python2.7/site-packages:/home/garaemon/gprog/scan-scripts:/home/garaemon/gprog/scan-scripts:/home/garaemon/gprog/scan-scripts"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/garaemon/ros/groovy/garaemon-ros-pkg/rwt_utils/rwt_plot/build/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/garaemon/ros/groovy/garaemon-ros-pkg/rwt_utils/rwt_plot:/opt/ros/groovy/share:/opt/ros/groovy/stacks"
